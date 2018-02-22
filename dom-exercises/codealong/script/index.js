8ec7b3a305c6071379674e4f52629f55a0ccc3bd
Linus Plume <plumee_91@hotmail.com>
2018-02-22 09:53:53 +0100

    No console log

--- dom-exercises/codealong/script/index.js
+++ dom-exercises/codealong/script/index.js
@@ -2,13 +2,27 @@ let message = document.getElementById("message");
 let button = document.getElementById("submit");
 let output = document.getElementById("messageOutput");
 let messages = [];
+let currentDate = new Date();
+
+currentDate.getFullYear();
+currentDate.getMonth();
+currentDate.getDate();
+currentDate.getHours();
+currentDate.getMinutes();
+currentDate.getSeconds();
+
+
 
 button.addEventListener("click", function () {
-    messages.push({ message: message.value });
+    messages.push({
+        message: message.value,
+        timeStamp: currentDate
+    });
     output.innerHTML = "";
     for(let message of messages){
         output.innerHTML += `Your messages: 
-        <p>${message.message}</p>
+        <p>Message: ${message.message}</p>
+        <p>Time: ${message.timeStamp}</p>
         `;
     }
     message.value = "";
