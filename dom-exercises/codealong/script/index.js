e034cabd146c047b0828d83a4f58bd33c33cddaf
Linus Plume <plumee_91@hotmail.com>
2018-02-22 09:05:01 +0100

    Make the latest message display

--- dom-exercises/codealong/script/index.js
+++ dom-exercises/codealong/script/index.js
@@ -1,9 +1,10 @@
 let message = document.getElementById("message");
 let button = document.getElementById("submit");
-
+let output = document.getElementById("messageOutput");
 let messages = [];
 
 button.addEventListener("click", function () {
     messages.push({ message: message.value });
-    console.log("messages array", messages);
+    output.innerText = message.value;
+    message.value = "";
 });
